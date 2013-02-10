e0d4d5a7e2da9b3d4ed897687740de7762994b44
Tony Myer <mediamaker@gmail.com>
2013-02-10 12:46:24 +0100

    Proper url for back to the tutorial link added

--- tables_download/js/main.js
+++ tables_download/js/main.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
 //If a table has more than 3 tds show visual scroll cue
 
-	$("table.responsive").each(function () { //change this number if needed ex. length > 2
+	$("table.responsive").each(function () {
     if ($("td", this).length > 3) {
         $(this).before("<div style='position:relative;'><span class='MobiScroll'></span></div>");
     	}
